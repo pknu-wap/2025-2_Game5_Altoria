de15383377cd7ec535562411cbde3e1c7050d4ff
Kimikhwan <moonfgg@naver.com>
2025-09-26 21:24:50 +0900

    Fix[FC_1]: Manager 오브젝트 배치 및 클래스 수정
    - Instance 프로퍼티에서 instance 생성 코드 삭제

--- Assets/Scripts/Manager/Manager.cs
+++ Assets/Scripts/Manager/Manager.cs
@@ -3,53 +3,35 @@ using UnityEngine;
 
 public class Manager : MonoBehaviour
 {
-    public static Manager Instance
-    {
-        get
-        {
-            if(instance == null)
-            {
-                GameObject ob = GameObject.Find("Manager");
-                if (ob == null)
-                    ob = new GameObject("Manager");
-                instance = Utils.GetOrAddComponent<Manager>(ob);
-                DontDestroyOnLoad(ob);
-            }
-            return instance;
-        }
-    }
+    private static Manager instance;
+    public static Manager Instance { get { return instance; } }
+
     public static UIController UI { get { return Instance.ui; } }
     public static TimeController Time { get { return Instance.time; } }
     public static SceneLoader SceneLoader { get { return Instance.scene; } }
 
-    private static Manager instance;
-
-    UIController ui;
-    TimeController time;
-    SceneLoader scene;
+    private UIController ui;
+    private TimeController time;
+    private SceneLoader scene;
 
     private void Awake()
     {
-        Init();
-
-        ui = new UIController();
-        time = new TimeController();
-        scene = new SceneLoader();
-    }
-
-    public void Init()
-    {
         if (instance == null)
         {
-            GameObject ob = GameObject.Find("Manager");
-            if (ob == null)
-                ob = new GameObject("Manager");
-            instance = Utils.GetOrAddComponent<Manager>(ob);
-            DontDestroyOnLoad(ob);
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            InitManagers();
         }
         else
         {
             Destroy(gameObject);
         }
+    }
+
+    private void InitManagers()
+    {
+        ui = new UIController();
+        time = Utils.GetOrAddComponent<TimeController>(gameObject);
+        scene = Utils.GetOrAddComponent<SceneLoader>(gameObject);
     }
 }
