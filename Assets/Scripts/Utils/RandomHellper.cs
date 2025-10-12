26b6724af8e0aedcd87a3c54790597ee75ff5b8b
Kimikhwan <moonfgg@naver.com>
2025-10-12 22:46:37 +0900

    Refactor[FC_1]: 랜덤 클래스 리펙토링

--- Assets/Scripts/Utils/RandomHellper.cs
+++ Assets/Scripts/Utils/RandomHellper.cs
@@ -1,52 +1,25 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class RandomHellper
 {
-    /// 확률(weight)을 가진 요소 중 하나를 선택.
-    private T Pick<T>(IEnumerable<(T item, float weight)> items)
+    public T Pick<T>(IList<(T item, float weight)> items)
     {
-        float total = 0;
-        foreach (var i in items)
-            total += i.weight;
+        float total = items.Sum(i => i.weight);
+        float ran = Random.Range(0, total);
+        float cumulative = 0;
 
         float rand = Random.Range(0, total);
 
-        foreach (var i in items)
+        for(int i = 0; i < items.Count(); i++)
         {
-            if (rand < i.weight)
-                return i.item;
-            rand -= i.weight;
+            cumulative += items[i].weight;
+            if (cumulative > ran)
+                return items[i].item;
+            cumulative += items[i].weight;
         }
 
         return default;
     }
-
-    // 채집 시스템
-    public string GetCollectItem(CollectInteractSO data, CollectToolSO tool)
-    {
-        if (data == null || data.drops == null || data.drops.Count == 0)
-        {
-            Debug.LogWarning($"{GetType()} : 데이터가 없습니다.");
-            return null;
-        }
-
-        var list = new List<(string, float)>();
-
-        foreach (var drop in data.drops)
-        {
-            float prob = drop.baseProbability;
-            if (tool != null && tool.type == data.collectType)
-                prob += prob * tool.rareBonus;
-
-            list.Add((drop.itemID, prob));
-        }
-
-        return Pick(list);
-    }
-
-    // 강화 시스템
-    public string GetUpgradeResult() { return null; }
-    // 재작 시스템
-    public string GetCrateBigResult() { return null; }
 }
